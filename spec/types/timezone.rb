de8ec4cb7f88d13db07274f4c1517b9e7284beb3
efass <efass@verisign.com>
2017-07-17 22:08:00 +0000

    tests that there is a version of timezone test

--- spec/types/timezone.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Serverspec::TimeZone do
- it 'has correct timezone' do
-  expect (Serverspec::TimeZone::VERSION).not_to be nil
- end
-end
--- spec/types/timezone_spec.rb
+++ spec/types/timezone_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Serverspec::TimeZone do
  it 'has correct timezone' do
-  expect (Serverspec::TimeZone::VERSION).not_to be nil
+  expect(Serverspec::TimeZone::VERSION).to eq('0.0.1')
  end
 end
